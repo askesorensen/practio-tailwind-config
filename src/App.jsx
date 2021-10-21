590cfb13006f80a3d1556069e1681f90a7e3c8a2
aske <hello@aske.co>
2021-10-21 16:17:26 +0200

    added border radius & box shadow

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,56 @@
 function App() {
   return (
-    <main className="h-screen bg-sky-light p-xxlarge">
+    <main className='h-screen bg-sky-light p-xxlarge'>
       {/* Section: Spacing Scale */}
-      <section className="bg-white rounded-3xl p-xlarge">
-        <h1 className="text-xl font-bold text-blue">
-          Spacing scale
-        </h1>
+      <section className='bg-white rounded-xlarge p-xlarge'>
+        <h1 className='text-2xl font-bold'>Spacing scale</h1>
 
-        <span>xxsmall</span>
-        <div className="bg-blue h-medium w-xxsmall"></div>
-        
+        <div className='flex flex-col mt-large'>
+          <div className='flex items-center border-b gap-medium border-sky'>
+            <span className='w-huge'>xxsmall</span>
+            <div className='bg-blue h-medium w-xxsmall'></div>
+          </div>
+
+          <div className='flex items-center border-b gap-medium border-sky'>
+            <span className='w-huge'>xsmall</span>
+            <div className='bg-blue h-medium w-xsmall'></div>
+          </div>
+          <div className='flex items-center border-b gap-medium border-sky'>
+            <span className='w-huge'>small</span>
+            <div className='bg-blue h-medium w-small'></div>
+          </div>
+          <div className='flex items-center border-b gap-medium border-sky'>
+            <span className='w-huge'>medium</span>
+            <div className='bg-blue h-medium w-medium'></div>
+          </div>
+          <div className='flex items-center border-b gap-medium border-sky'>
+            <span className='w-huge'>large</span>
+            <div className='bg-blue h-medium w-large'></div>
+          </div>
+          <div className='flex items-center border-b gap-medium border-sky'>
+            <span className='w-huge'>xlarge</span>
+            <div className='bg-blue h-medium w-xlarge'></div>
+          </div>
+          <div className='flex items-center border-b gap-medium border-sky'>
+            <span className='w-huge'>xxlarge</span>
+            <div className='bg-blue h-medium w-xxlarge'></div>
+          </div>
+          <div className='flex items-center border-b gap-medium border-sky'>
+            <span className='w-huge'>xxxlarge</span>
+            <div className='bg-blue h-medium w-xxxlarge'></div>
+          </div>
+          <div className='flex items-center border-b gap-medium border-sky'>
+            <span className='w-huge'>huge</span>
+            <div className='bg-blue h-medium w-huge'></div>
+          </div>
+          <div className='flex items-center border-b gap-medium border-sky'>
+            <span className='w-huge'>xhuge</span>
+            <div className='bg-blue h-medium w-xhuge'></div>
+          </div>
+        </div>
       </section>
+
+      <section></section>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,23 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    // Our spacing scale is based on a 8-pixel grid, but 
-    // in some cases it can be subdivided into smaller sizes.
+    // SPACING
+    // Our spacing scale is based on a 8-pixel grid, but in some cases it can be subdivided into smaller sizes.
     spacing: {
-      'none': '0px',
-      'xxsmall':'4px',
-      'xsmall': '8px',
-      'small': '12px',
-      'medium' : '16px',
-      'large': '24px',
-      'xlarge': '32px',
-      'xxlarge': '48px',
-      'xxxlarge': '64px',
-      'huge': '96px',
-      'xhuge': '128px'
+      none: '0rem',
+      xxsmall: '0.25rem', // 4px
+      xsmall: '0.5rem', // 8px
+      small: '0.75rem', // 12px
+      medium: '1rem', // 16px
+      large: '1.5rem', // 24px
+      xlarge: '2rem', // 32px
+      xxlarge: '3rem', // 48px
+      xxxlarge: '4rem', // 64px
+      huge: '6rem', // 96px
+      xhuge: '8rem', // 128px
     },
+
+    // COLORS
     colors: {
       //utility
       transparent: 'transparent',
@@ -27,20 +29,20 @@ module.exports = {
         lighter: '#F9FAFB',
         light: '#F4F6F8',
         DEFAULT: '#DFE3E8',
-        dark: '#C4CDD5'
+        dark: '#C4CDD5',
       },
       ink: {
         lightest: '#919EAB',
         lighter: '#637381',
         light: '#3D4754',
-        DEFAULT: '#192029'
+        DEFAULT: '#192029',
       },
       blue: {
         lighter: '#E5EEFF',
         light: '#B2CDFF',
         DEFAULT: '#1161FB',
         dark: '#0439CC',
-        darker: '#001640'
+        darker: '#001640',
       },
       // Interaction colors
       success: '#27CC6A',
@@ -49,8 +51,36 @@ module.exports = {
 
       // etc
       white: '#FFFFFF',
-      }
     },
+
+    // FONT FAMILY
+    fontFamily: {
+      sans: ['Inter var', 'ui-sans-serif', 'system-ui'],
+      mono: ['ui-monospace', 'SFMono-Regular'],
+    },
+
+    // BORDER RADIUS
+    borderRadius: {
+      none: '0',
+      xsmall: '0.125rem', // 2px
+      small: '0.25rem', // 4px
+      DEFAULT: '0.375rem', // 6px
+      large: '0.5rem', // 8px
+      xlarge: '0.75rem', // 12px
+      xxlarge: '1.75rem', // 20px
+      full: '9999px',
+    },
+
+    // BOX SHADOWS
+    boxShadows: {
+      small: '0px 1px 2px rgba(25, 32, 41, 0.15)',
+      DEFAULT:
+        '0px 1px 3px rgba(25, 32, 41, 0.15), 0px 1px 2px rgba(25, 32, 41, 0.05)',
+      md: '0px 2px 16px rgba(25, 32, 41, 0.08), 0px 1px 3px rgba(25, 32, 41, 0.1)',
+      lg: '0px 31px 41px rgba(25, 32, 41, 0.1), 0px 2px 16px rgba(25, 32, 41, 0.08)',
+      none: 'none',
+    },
+  },
   variants: {
     extend: {},
   },
